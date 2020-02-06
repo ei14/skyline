02406f6470eb4a9e436561382cfd345de8267ef0
ei14 <tbk283@gmail.com>
2020-02-06 09:43:27 -0600

    Firebase integration attempt

--- platforms/android/app/src/main/java/run/ace/IHaveProperties.java
+++
@@ -1,9 +0,0 @@
-//-------------------------------------------------------------------------------------------------------
-// Copyright (C) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
-//-------------------------------------------------------------------------------------------------------
-package Windows.UI.Xaml.Controls;
-
-public interface IHaveProperties {
-		void setProperty(String propertyName, Object propertyValue);
-}
--- platforms/ios/Skyline/Plugins/cordova-plugin-ace/ImageSource.h
+++
@@ -1,9 +0,0 @@
-//-------------------------------------------------------------------------------------------------------
-// Copyright (C) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
-//-------------------------------------------------------------------------------------------------------
-@interface ImageSource : NSObject
-
-@property NSString* UriSource;
-
-@end
