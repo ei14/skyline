774b390ff5b1488005b1d22431288ec3d60b2642
ei14 <tbk283@gmail.com>
2020-02-06 15:33:56 -0600

    FINALLY GOT NOTIFS TO WORK

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-firebasex.FirebasePlugin",
+      "file": "plugins/cordova-plugin-firebasex/www/firebase.js",
+      "pluginId": "cordova-plugin-firebasex",
+      "clobbers": [
+        "FirebasePlugin"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-androidx": "1.0.2",
+    "cordova-plugin-androidx-adapter": "1.1.0",
+    "cordova-plugin-firebasex": "7.0.2",
+    "cordova-plugin-whitelist": "1.3.4"
+  };
+});
